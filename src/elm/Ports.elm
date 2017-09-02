5f1633342651ef439a6d4c6fefb717f47c95c43e
ChadoNihi <chadoishere@gmail.com>
2017-09-02 18:54:43 +0300

    scaffolding

---
+++ src/elm/Ports.elm
@@ -0,0 +1,4 @@
+port module Ports exposing (fetchUserData)
+
+
+port fetchUserData
---
+++ src/elm/Route.elm
@@ -0,0 +1,7 @@
+module Route exposing (..)
+
+
+type Route
+    = AllQuotesRoute
+    | CurrReadingRoute
+    | ReviewedRoute
